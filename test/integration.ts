63f654a771be13621bfa5e90f70b9a7370170cd7
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2019-08-13 17:04:41 -0700

    more reliable tests, remove unnecessary test dependency

--- test/integration.ts
+++ test/integration.ts
@@ -1,7 +1,6 @@
 import assert from "assert";
 import temp from "temp";
 import pify from "pify";
-import EthCrypto, { Encrypted } from "eth-crypto";
 
 import { Server } from "..";
 import { Client } from "..";
@@ -13,24 +12,29 @@ const mktmp = pify(temp.mkdir);
 
 // tslint:disable-next-line: interface-name
 interface Identity {
-  privateKey: string;
-  publicKey: string;
   address: string;
 }
 
+// tslint:disable-next-line: interface-name
+interface FakeEncryptedMessage {
+  plaintext: string;
+}
+
 describe("end-to-end integration", () => {
-  let server: Server<Encrypted>;
+  let server: Server<FakeEncryptedMessage>;
   const numClients = 5;
-  const clients: Array<Client<Encrypted>> = [];
+  const clients: Array<Client<FakeEncryptedMessage>> = [];
   const identities: Identity[] = [];
 
   beforeAll(async () => {
     const dbPath = await mktmp("end-to-end-test");
-    server = new Server<Encrypted>(dbPath);
+    server = new Server<FakeEncryptedMessage>(dbPath);
 
     for (let i = 0; i < numClients; i++) {
-      identities.push(await EthCrypto.createIdentity());
-      const client = new Client<Encrypted>("http://127.0.0.1:8000");
+      identities.push({
+        address: `000000000000000000000000000000000000000${i}`
+      });
+      const client = new Client<FakeEncryptedMessage>("http://127.0.0.1:8000");
       clients.push(client);
     }
   });
@@ -43,44 +47,44 @@ describe("end-to-end integration", () => {
 
   test("send to everyone", async () => {
     let messageCount = 0;
-    const messages: { [index: string]: IMessage<Encrypted> } = {};
+    const messages: { [index: string]: IMessage<FakeEncryptedMessage> } = {};
+
+    const promises = [];
 
     // for clients 1..n, take the first message they receive and dump it into
     // the messages array
     for (let i = 1; i < numClients; i++) {
-      clients[i].once("message", (message: IMessage<Encrypted>) => {
-        messageCount++;
-        messages[identities[i].address] = message;
-      });
+      promises.push(new Promise((accept) => {
+        clients[i].once("message", (message: IMessage<FakeEncryptedMessage>) => {
+          messageCount++;
+          messages[identities[i].address] = message;
+          accept();
+        });
+      }));
     }
 
     // have the 0th client send a unique message to clients 1..n
     for (let i = 1; i < numClients; i++) {
-      const encryptedMessage = await EthCrypto.encryptWithPublicKey(
-        identities[i].publicKey,
-        `Hello there client 0x${identities[i].address}!`,
-      );
+      const fakeEncryptedMessage = {
+        plaintext: `Hello there client 0x${identities[i].address}!`,
+      }
 
-      const message: IMessage<Encrypted> = {
+      const message: IMessage<FakeEncryptedMessage> = {
         to: identities[i].address,
-        message: encryptedMessage,
+        message: fakeEncryptedMessage,
       };
 
       clients[0].sendMessage(message);
     }
 
-    // add a short delay so the messages can fly around and do their thing
-    await new Promise((resolve) => setTimeout(resolve, 150));
+    await Promise.all(promises);
 
     assert.strictEqual(messageCount, numClients - 1);
 
     for (let i = 1; i < numClients; i++) {
-      const decryptedMessage = await EthCrypto.decryptWithPrivateKey(
-        identities[i].privateKey,
-        messages[identities[i].address].message,
-      );
+      const fakeDecryptedMessage = messages[identities[i].address].message.plaintext;
 
-      assert.strictEqual(decryptedMessage, `Hello there client 0x${identities[i].address}!`);
+      assert.strictEqual(fakeDecryptedMessage, `Hello there client 0x${identities[i].address}!`);
     }
   });
 
